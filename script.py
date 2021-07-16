85b5c6791075c14d9fcd15aadd036404325b4886
ryankrebs016 <33184695+ryankrebs016@users.noreply.github.com>
2021-07-15 22:46:26 -0400

    update

--- script.py
+++ script.py
@@ -1,106 +1,118 @@
 import time
 import numpy as np
 import serial
-import RPi.GPIO as gpio
+try:
+    import RPi.GPIO as gpio
+except:
+    print('pi modules not loaded')
+import struct
+
 
 #call this script when the pi is started up. Then use the switch to turn on pulsing.
 
 
-pulser_port=''
-pulser_baud=1600
+pulser_port='serial0'
+pulser_baud=112500
 timeout=1
 
 #make class for switch to clean up code a little
-def switch():
-    pin=18
-    gpio.setmode(gpio.BCM)
-    gpio.setup(pin,gpio.INPUT)
-    def read_switch():
-        return gpio.INPUT(pin)
+class switch():
+    def __init__(self):
+        self.pin=18
+        gpio.setmode(gpio.BCM)
+        gpio.setup(self.pin,gpio.INPUT)
+    def read_switch(self):
+        return gpio.INPUT(self.pin)
 
-def gps_comms():
+class gps_comms():
     print('holding')
     time=0
     coords=0
 
 #make class for commuication with the pulsers
-def pulser_comms():
-    amplitude=1000
-    period=100000
-    read_write=1
-    flag='0110'
-    pulser=serial.Serial(pulser_port,pulser_baud,timeout=1)
-    start_time=0
+class pulser_comms():
+    def __init__(self):
+        self.amplitude=1000
+        self.period=100000
+        self.read_write=1
+        self.flag='0110'
+        #self.pulser=serial.Serial(pulser_port,pulser_baud,timeout=1)
+        self.start_time=0
+        self.code='bbbbHIb'
 
     #build packet to send to the pulser
-    def build_pulsar_packet(amplitude,period,read_write,flag):
-        to_send=[]
+    def build_pulsar_packet(self,amplitude,period,read_write,flag):
+        
         preamble=0x55
-        size=0x9
+        size=0x09
         command_code=0x01
         io=0b00000000
         if(read_write==1):
             io=0b00000001
-        amp_bits=convert_voltage_to_binary(amplitude)
-        per_bits=convert_period_to_binary(period)
+        amp_bits=self.convert_voltage_to_dec(amplitude)
+        per_bits=self.convert_period_to_dec(period)
         flag
         #handle crc
-        to_send=[preamble,size,command_code,io,amp_bits,per_bits,flag_bits,crc]
+        to_send=struct.pack(self.code,preamble,size,command_code,io,amp_bits,per_bits,flag)
+        
         return to_send
     
     def build_flag_byte(flag):
         return
-    def convert_voltage_to_binary(amplitude):
+    def convert_voltage_to_dec(self,amplitude):
         if(amplitude<1000):
             print('below range')
-            return '{0:016b}'.format(0)
+            return 0
         if(amplitude>2000):
             print('above range')
-            return '{0:016b}'.format(65535)
-        bit_value=65536/1000 * (amplitude-1000)
-        binary_val='{0:016b}'.format(bit_value)
-        return binary_val
+            return 2**16-1
+        value=int(65536/1000 * (amplitude-1000))
+        
+        return value
     
-    def convert_binary_to_voltage(val):
-        i=int(val,2)
-        voltage=1000/65536 * i  +1000
+    def convert_dec_to_voltage(self,val):
+        
+        voltage=1000/65536 * val  +1000
         return voltage
 
-    def convert_period_to_binary(period):
+    def convert_period_to_dec(self,period):
         if(period<1000):
             print('below range')
-            return '{0:032b}'.format(0)
+            return 0
         if(period>500000):
             print('above range')
-            return '{0:032b}'.format(2**32 -1)
-        bit_value=2**32 /(500000-1000) * (period-1000)
-        binary_val='{0:032b}'.format(bit_value)
-        return binary_val
+            return 2**32-1
+        value=int(2**32 /(500000-1000) * (period-1000))
         
-    def convert_binary_to_period(val):
-        i=int(val,2)
-        period=(500000-1000)/10**32 * i  +1000
+        return value
+        
+    def convert_dec_to_period(self,val):
+        
+        period=(500000-1000)/10**32 * val  +1000
         return period
     
     #read packet returned from the pulser, either after send read command or from the normal response
-    def read_pulser_response(response):
-        print('holding')
+    def read_pulser_response(self,response):
+        rec=struct.unpack(self.code,response)
+        print(self.convert_dec_to_voltage(rec[4]),'V')
+        print(self.convert_dec_to_period(rec[5]),'uS')
+
         #create general function to read the contents of a returned packet
-    def turn_on():
-        pulser.write(build_pulsar_packet(amplitude,period,1,'0110'))
+    def turn_on(self):
+        self.pulser.write(self.build_pulsar_packet(self.amplitude,self.period,1,'0110'))
         start_time=time()
-        time.sleep(.001)
-        returned_bytes=pulser.read()
-        read_pulser_response(returned_bytes)
+        time.sleep(.1)
+        returned_bytes=self.pulser.read()
+        self.read_pulser_response(returned_bytes)
 
-    def turn_off():
-        pulser.write(build_pulsar_packet(amplitude,period,1,'0000'))
+    def turn_off(self):
+        self.pulser.write(self.build_pulsar_packet(self.amplitude,self.period,1,'0000'))
 
-    def request_info():
-        pulser.write(build_pulsar_packet(amplitude,period,0,'0000'))
-        time.sleep(.001)
-        returned_bytes=pulser.read()
-        read_pulser_response(returned_bytes)
+    def request_info(self):
+        self.pulser.write(self.build_pulsar_packet(self.amplitude,self.period,0,'0000'))
+        time.sleep(.1)
+        returned_bytes=self.pulser.read()
+        self.read_pulser_response(returned_bytes)
     
 
 if __name__=='__main__':
