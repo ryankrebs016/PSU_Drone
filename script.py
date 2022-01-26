895872c26895a1aa29bca6034a96f52d0b243b91
ryankrebs016 <33184695+ryankrebs016@users.noreply.github.com>
2022-01-26 01:43:49 -0500

    fixed rx and tx packets + RS485 module controls

--- script.py
+++ script.py
@@ -1,14 +1,13 @@
+from hashlib import new
 from sys import byteorder
 import time
 import numpy as np
 import serial
+import RPi.GPIO as gpio
 try:
-    import RPi.GPIO as gpio
     from picamera import PiCamera
 except:
     print('pi modules not loaded')
-
-
 
 #call this script when the pi is started up. Then use the switch to turn on pulsing.
 
@@ -18,12 +17,11 @@ pulser_baud=112500 #define pulser baud rate. ie frequency in which pi sends out 
 timeout=1
 
 def main():
-    print('main')
     coms=pulser_comms() #initalize pulser comms
     coms.amplitude=1000 #amplitude of pulse in V
-    coms.period=10**6 #time between pulses in micro s when using pulser's timing
+    coms.period=5E5 #time between pulses in micro s when using pulser's timing
+    print('Voltage set to %s V. Period set to %s uS'%(coms.amplitude,coms.period))
     coms.turn_on() #run class function which turns on pulser and enables pulsing
-    time.sleep(10) #let run for a few seconds to obsrve pulses on scope
     coms.turn_off() #shuts off pulsing
     exit() #testing comms to pulser for now
     ins=inputs() #initialize switch class
@@ -63,12 +61,14 @@ def camera():
 #make class for switch to clean up code a little
 class inputs():
     def __init__(self):
-        self.switch_pin=18
-        self.pps_pin=12
+        self.switch_pin=4
+        self.pps_pin=25
+       
         gpio.setmode(gpio.BCM)
         gpio.setup(self.switch_pin,gpio.INPUT)
         gpio.setup(self.pps_pin,gpio.INPUT)
-        gpio.add_event_detect(self.pps_pin,gpio.RISING,callback=triggered,bouncetime=.1)
+       
+        gpio.add_event_detect(self.pps_pin,gpio.RISING,callback=self.triggered(),bouncetime=.1)
     def read_switch(self):
         return gpio.INPUT(self.pin)
     def triggered():
@@ -76,7 +76,6 @@ class inputs():
 
 
 class gps_comms():
-    print('holding')
     time=0
     coords=0
 
@@ -86,10 +85,24 @@ class pulser_comms():
         self.amplitude=1000 #amplidue of pulses
         self.period=100000 #period between pulses
         self.read_write=1 #define read or write bit
-        self.flag='0101' #defines flag bits 'abcd' a=read only vcc error, b=enable pulsing, enable eternal triggering, enable high voltage supply
+        #self.flag='0101' #defines flag bits 'abcd' a=read only vcc error, b=enable pulsing, enable eternal triggering, enable high voltage supply]
+        self.flag=0b00000101
         self.pulser=serial.Serial(pulser_port,pulser_baud,timeout=1)
+
         self.start_time=0
-        
+        self.pulser_rx_pin=23
+        self.pulser_tx_pin=24
+        gpio.setup(self.pulser_rx_pin,gpio.OUT,initial=gpio.high)
+        gpio.setup(self.pulser_tx_pin,gpio.OUT,initial=gpio.low)
+
+    def set_RS_tx(self):
+        gpio.output(self.pulser_rx_pin, gpio.low)
+        gpio.output(self.pulser_tx_pin,gpio.low)
+        time.sleep(.05)
+    def set_RS_rx(self):
+        gpio.output(self.pusler_rx_pin,gpio.high)
+        gpio.output(self.pulser_tx_pin,gpio.high)
+        time.sleep(0.05)
 
     #build packet to send to the pulser
     def build_pulsar_packet(self,amplitude,period,read_write,flag):
@@ -100,31 +113,23 @@ class pulser_comms():
         io=0b00000000 #defined byte for input or output status
         if(read_write==1):
             io=0b00000001
+
         amp_dec=self.convert_voltage_to_dec(amplitude) #gets the int value corresponding to a voltage (V)
         amp_bits=amp_dec.to_bytes(2,byteorder='big')
         
         per_dec=self.convert_period_to_dec(period) #gets the int value corresponding to a period (uS)
         per_bits=per_dec.to_bytes(4,byteorder='big')
  
-        to_send_dec=[preamble,size,command_code,io,amp_bits,per_bits,flag]
-        to_send_hex=[]
-        for i in range(len(to_send_dec)-1):
-            if(len(to_send_dec[i])>1):
-                for i in range(len(to_send_dec[i])):
-                    to_send_hex.append(to_send_dec.to_bytes(1,byteorder='big'))
-            else:
-                to_send_hex.append(to_send_dec.to_bytes(1,byteorder='big'))
-
-        crc_int=self.crc16(to_send_dec,13)
+        to_send_dec=[preamble,size,command_code,io,amp_bits[0],amp_bits[1],per_bits[0],per_bits[1],per_bits[2],per_bits[3],flag]
+   
+        crc_int=self.crc16(to_send_dec,11)
         crc_hex=crc_int.to_bytes(2,byteorder='big')
-        to_send_hex.append(crc_hex)
+        to_send_dec.append(crc_hex[0])
+        to_send_dec.append(crc_hex[1])
 
-        return to_send #return array which should be used in serial.write()
+        return to_send_dec #return array which should be used in serial.write()
 
     def crc16(self,data, no):
-        if(type(data[0])==bytes):
-            for i in range(np.size(data)):
-                data[i]=int.from_bytes(data[i],byteorder='big')
         crc = 0xffff
         poly = 0xa001               # Polynomial used for Modbus RS485 applications
         temp = no
@@ -137,18 +142,12 @@ class pulser_comms():
                 if crc & 0x0001:
                     crc = (crc>>1) ^ poly
                 else:
-                    crc >>= 1
-                            
+                    crc >>= 1           
             no -= 1
-
             if no == 0:
                 break
 
         return crc & 0xffff
-
-
-    def build_flag_byte(self,flag): #empty for now. If we need more control over flag byte this can be created
-        return 0
 
     def convert_voltage_to_dec(self,amplitude): #gets unsigned short int value from a voltage
         if(amplitude<1000):
@@ -179,32 +178,62 @@ class pulser_comms():
         
     def convert_dec_to_period(self,val): #converts unsigned int value to period
         
-        period=(500000-1000)/10**32 * val  +1000
+        period=(500000-1000)/2**32 * val  +1000
         return period
     
     #read packet returned from the pulser, either after send read command or from the normal response
     def read_pulser_response(self,response): #unpacks data from response and print ampltiude and period to CL
+        voltage_packet=int.from_bytes(response[4:5],byteorder='big'])
+        period_packet=int.from_bytes(response[6:10],byteorder='big')
         
-        print(self.convert_dec_to_voltage(int.from_bytes(response[4:5],byteorder='big')),'V')
-        print(self.convert_dec_to_period(int.from_bytes(response[6:9],byteorder='big')),'uS')
+        print(self.convert_dec_to_voltage(voltage_packet,' V'))
+        print(self.convert_dec_to_period(period_packet,' uS'))
 
         #create general function to read the contents of a returned packet
-    def turn_on(self):#will be used to turn on pulsing w/ pulser period
-        self.pulser.write(self.build_pulsar_packet(self.amplitude,self.period,1,0b00000101))
-        start_time=time()   
-        time.sleep(.5)
-        returned_bytes=self.pulser.read()
+    def turn_on(self,ext_trigger):#will be used to turn on pulsing w/ pulser period\
+        if(ext_trigger==1):
+            self.flag=0b00000111
+        send_packet=self.build_pulsar_packet(self.amplitude,self.period,1,self.flag)
+        print('Sending packet: ',send_packet)
+        self.set_RS_tx()
+        self.pulser.write(send_packet)
+         
+        time.sleep(.1)
+        self.set_RS_rx()
+        returned_bytes=self.pulser.read(13)
+        print('Returning packet: ',returned_bytes)
         self.read_pulser_response(returned_bytes)
+        
 
     def turn_off(self): #turns off puslsing
+        self.set_RS_tx()
         self.pulser.write(self.build_pulsar_packet(self.amplitude,self.period,1,0b00000000))
 
     def request_info(self):#sends byte array to request info from the pulser
-        self.pulser.write(self.build_pulsar_packet(self.amplitude,self.period,0,0b00000000))
+        self.set_RS_tx()
+        self.pulser.write(self.build_pulsar_packet(self.amplitude,self.period,0,self.flag))
         time.sleep(.5)
+        self.set_RS_rx()
         returned_bytes=self.pulser.read(13)
         self.read_pulser_response(returned_bytes)
-    
+    def adjust_voltage_and_period(self):
+        new_voltage=input('Input new voltage between 1000 and 2000 V: ')
+        new_period=input('Input new period between 1000 and 5E5 uS: ')
+
+        self.amplitude=new_voltage
+        self.period=new_period
+
+        send_packet=self.build_pulsar_packet(self.amplitude,self.period,1,self.flag)
+        print('Sending packet: ',send_packet)
+        self.set_RS_tx()
+        self.pulser.write(send_packet)
+         
+        time.sleep(.1)
+        self.set_RS_rx()
+        returned_bytes=self.pulser.read(13)
+        print('Returning packet: ',returned_bytes)
+        self.read_pulser_response(returned_bytes)
+
 
 if __name__=='__main__':
     main()
